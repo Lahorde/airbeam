3756fe7f025fe4591dec1395ec54420e2eadfbc8
PINCENT Remi <remi.pincent@inria.fr>
2017-08-08 09:06:07 +0200

    Update firmware. Add hw timer, modify measure timing, clean code

--- firmware/arduino/AirBeam/AirBeam.ino
+++ firmware/arduino/AirBeam/AirBeam.ino
@@ -1,49 +1,284 @@
+/******************************************************************************
+ * @file    airbeam.ino
+ * @author  Rémi Pincent - INRIA
+ * @date    29/07/2017
+ *
+ * @brief AirBeam firmware. Communicates over BT to provide some sensors value 
+ * (default : NO2, Temperature, PM2.5)
+ * Original code from : https://github.com/HabitatMap/AirCastingAndroidClient/tree/master/arduino/aircasting
+ * Project : AirBeam
+ * Contact:  Rémi Pincent - remi.pincent@inria.fr
+ *
+ * Revision History:
+ * 
+ * LICENSE :
+ * AirBeam (c) by Rémi Pincent
+ * AirBeam is licensed under a
+ * Creative Commons Attribution-NonCommercial 3.0 Unported License.
+ *
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-nc/3.0/>.
+ *****************************************************************************/
+
+/**************************************************************************
+ * Include Files
+ **************************************************************************/
 #include <SoftwareSerial.h>
 #include <DHT.h>
 #include <stdlib.h>
 #include <cairsens.h>
 #include <logger_config.h>
+#include <TimerOne.h>
+
+/**************************************************************************
+ * Macros
+ **************************************************************************/
+/** Temperature provided in Celcius, if not defined temp in fahrenheit */
+#define CELSIUS_TEMP
 
 #define DHTPIN 9
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
-#define LOG_LEVEL LOG_LEVEL_DEBUG
+#define LEDPIN 13
+#define BT_BUF_LEN 50
+/**************************************************************************
+ * Manifest Constants
+ **************************************************************************/
+static const uint16_t BT_PUBLISH_PERIOD = 1000;
+static const uint16_t BT_STATUS_PERIOD  = 100;
+static const uint16_t NO2_SAMPLE_TIME   = 500;
+static const uint16_t PM_SAMPLE_TIME    = 200;
+static const uint16_t LED_BLINK_PERIOD  = 200;
+/**************************************************************************
+ * Type Definitions
+ **************************************************************************/
+typedef enum{
+  NOT_CONNECTED = -1,           /** never been connected */
+  NOT_CONNECTED_AFTER_CONN = 0, /** not connected (but been connected) */
+  CONNECTED = 1,
+}EBTStatus;
+/**************************************************************************
+ * Global Variables
+ **************************************************************************/
 
-int humi, cel, fah, kelv; 
-unsigned long analogcount;
-double analogtotal;
+/**************************************************************************
+ * Static Variables
+ **************************************************************************/
+static String airbeam_mac = "";
+static unsigned long bt_publish_time = 0;
+static unsigned long pm_sum = 0;
+static uint16_t pm_nbmeas;
+static unsigned long pm_starttime = 0;
 
-String inData;
-unsigned long duration;
-unsigned long starttime = 0;
-unsigned long sampletime_ms = 1000;
-unsigned long analogvalue = 0;
-double hppcf = 0;
-double ugm3 = 0;
+static unsigned long no2_sum = 0;
+static uint16_t no2_nbmeas = 0;
+static unsigned long no2_starttime = 0;
 
-DHT dht(DHTPIN, DHTTYPE);
+static unsigned long bt_status_time = 0;
+static volatile bool led_status = LOW;
+
+static EBTStatus bt_connected = NOT_CONNECTED;
+char bt_index = 0;
+char bt_buffer[BT_BUF_LEN];
+
+static DHT dht(DHTPIN, DHTTYPE);
 
 /**
  Not all pins on the Leonardo and Micro support change interrupts,
  so only the following can be used for RX:
  8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+ Moreover, multiple instances can be created and assign several soft serial ports is possible,
+ but only 1 can be activated to send/receive at a time.
 */
-SoftwareSerial mySerial(10, 11); //RX, TX
+static SoftwareSerial bt_serial(10, 11); //RX, TX
 /** Use MOSI/SCK pins from external 6 pins connector CON_ICSP */
-SoftwareSerial cairsensSerial(16, 15); //RX, TX
-
-#define LEDPIN 13
-#define BT_BUF_LEN 10
-char bt_index = 0;
-char bt_buffer[BT_BUF_LEN];
-int8_t bt_connected = -1; // 0 = not connected (but been connected), 1 == connected, -1 == never been connected
+static SoftwareSerial cairsens_serial(16, 15); //RX, TX
 
 /** Communicate with Cairsens N0X sensor over UART */
-static CairsensUART cairsens(&cairsensSerial);
-/** Sample time in ms */
-static const unsigned long NOX_SAMPLE_TIME = 100;
-static unsigned long nox_starttime = 0;
+static CairsensUART cairsens(&cairsens_serial);
 
+/**************************************************************************
+ * Global Functions Declarations
+ **************************************************************************/
+ 
+ /**************************************************************************
+ * Static Functions Declarations
+ **************************************************************************/
+static void initializeBT(void);
+static uint8_t put_chr_buf(char c);
+static void analyze_buf(void);
+static void update_bt_status(void);
+static void readBTMAC(void);
+/** Called under interrupted context */
+static void statusLedUpdate(void);
+/**************************************************************************
+ * Global Functions Definitions
+ **************************************************************************/
+void setup() {
+  delay(3000);
+  
+  Serial.begin(115200);
+  LOG_INIT(&Serial);
+
+  /** Initialize Cairsens UART comm */
+  cairsens_serial.begin(9600);
+
+  initializeBT();
+
+  Timer1.initialize((uint32_t) LED_BLINK_PERIOD*1000);
+  Timer1.attachInterrupt(statusLedUpdate); 
+  pinMode(LEDPIN, OUTPUT);
+  led_status = LOW;
+  digitalWrite(LEDPIN, led_status);
+  
+  pm_starttime = millis();
+  no2_starttime = millis();
+  bt_publish_time = millis();
+  bt_status_time = millis();
+
+  /** Initially BT not connected */
+  Timer1.start();
+}
+
+void loop() { 
+  if(millis() - bt_status_time > BT_STATUS_PERIOD)
+  {
+      update_bt_status();
+      bt_status_time = millis();
+  }  
+
+  if ((millis() - bt_publish_time) > BT_PUBLISH_PERIOD)
+  {
+    bt_publish_time = millis(); // moved to create more regular periods.
+    double loc_pm = (double)pm_sum/pm_nbmeas;
+    loc_pm = (loc_pm*5.0)/1024;
+    double loc_hppcf = (240.0*pow(loc_pm,6) - 2491.3*pow(loc_pm,5) + 9448.7*pow(loc_pm,4) - 14840.0*pow(loc_pm,3) + 10684.0*pow(loc_pm,2) + 2211.8*(loc_pm) + 7.9623);
+    double loc_ugm3 = .518 + .00274 * loc_hppcf;
+    if(loc_ugm3 < 0)
+    {
+      loc_ugm3 = 0;
+    }
+
+    int loc_humi = dht.readHumidity();
+    int loc_cel = dht.readTemperature();
+  
+    LOG_INFO_LN(F("\n%l - AirBeam MAC: %s - Temp: %f°F %d°C - Hum : %dRH - PM2.5 : hppcf: %f  ugm^3: %f - NO2 Gas: %fppm"), 
+      millis(),
+      airbeam_mac.c_str(),
+      (float)(loc_cel * 9)/5 + 32,
+      loc_cel,
+      loc_humi,
+      loc_hppcf,
+      loc_ugm3,
+      no2_sum/(10.0*no2_nbmeas));
+    
+    if(bt_connected == CONNECTED){
+      bt_serial.print(loc_ugm3);
+      bt_serial.print((";AirBeam:"));
+      bt_serial.print(airbeam_mac);
+      bt_serial.print((";AirBeam-PM;Particulate Matter;PM;micrograms per cubic meter;µg/m³;0;12;35;55;150"));
+      bt_serial.print("\n");
+
+#ifdef CELSIUS_TEMP
+      bt_serial.print(loc_cel);
+      bt_serial.print((";AirBeam:"));
+      bt_serial.print(airbeam_mac);
+      bt_serial.print((";AirBeam-C;Temperature;F;degrees Celcius;C;-17;-4;10;24;38"));
+#else /** Temperature in fahrenheit */
+      bt_serial.print(((loc_cel * 9)/5) + 32);
+      bt_serial.print((";AirBeam:"));
+      bt_serial.print(airbeam_mac);
+      bt_serial.print((";AirBeam-F;Temperature;F;degrees Fahrenheit;F;0;25;50;75;100"));
+#endif
+      bt_serial.print("\n");
+      
+      bt_serial.print(loc_humi);
+      bt_serial.print((";AirBeam:"));
+      bt_serial.print(airbeam_mac);
+      bt_serial.print((";AirBeam-RH;Humidity;RH;percent;%;0;25;50;75;100"));
+      bt_serial.print("\n");
+      
+      bt_serial.print(no2_sum/(10.0*no2_nbmeas));
+      bt_serial.print((";AirBeam:"));
+      bt_serial.print(airbeam_mac);
+      bt_serial.print(";Cairsens A40-0020-B;Azote dioxyde;N02;micrograms per cubic meter;µg/m³;0;12;35;55;150");
+      bt_serial.print("\n");
+     
+      no2_sum = 0;
+      no2_nbmeas = 0;
+
+      pm_sum = 0;
+      pm_nbmeas = 0;
+    }
+  }
+  
+  if ((millis() - no2_starttime) > NO2_SAMPLE_TIME)
+  {
+     /** Switch RX to Cairsens UART comm, only on SoftwareSerial instance can receive at a time
+     after this call, no data can be received by bt_serial instance */
+    cairsens_serial.listen();
+    
+    uint8_t nox_val = 0;
+    if(cairsens.getInstantValue(nox_val) == CairsensUART::NO_ERROR)
+    {
+      LOG_INFO_LN(F("%l - current NOX value = %fppm"), millis(), nox_val/10.0);
+      no2_nbmeas++;
+      no2_sum += nox_val;
+    }
+    else
+    {
+      LOG_ERROR(F("Could not get Cairsens NOX value"));
+    }
+      /** Switch RX to BT UART comm, only on SoftwareSerial instance can receive at a time
+     after this call, no data can be received by cairsens_serial instance */
+    bt_serial.listen();
+    no2_starttime = millis();
+  }
+
+  if ((millis() - pm_starttime) > PM_SAMPLE_TIME)
+  {
+    /** Read PM val */
+    pm_sum = pm_sum + analogRead(0);
+    pm_nbmeas++;
+
+    pm_starttime = millis();
+  }
+}
+
+ /**************************************************************************
+ * Static Functions Definitions
+ **************************************************************************/
+void initializeBT(void)
+{
+  /** Initialize BT UART comm, only on SoftwareSerial instance can receive at a time
+  after this call, no data can be received by cairsens_serial instance */
+  bt_serial.begin(115200);
+  bt_serial.println("BC:BR=08");
+  delay(100);
+  
+  bt_serial.begin(19200);
+  bt_serial.println("BC:BR=08");
+  delay(100);
+  
+  bt_serial.begin(9600);
+  bt_serial.println("BC:BR=08");
+  delay(100);
+  
+  bt_serial.begin(19200);
+  
+  bt_serial.println("BC:FT=00,00,00,01,03,0000");  // It will never try to autoconnect and will always be in discoverable mode.
+  delay(100);
+  bt_serial.println(("BC:AD"));
+  delay(100);
+  readBTMAC();
+
+  LOG_INFO_LN(F("AirBEAM BT MAC : %s"), airbeam_mac.c_str());
+  
+  bt_serial.print(("BC:NM=AirBeam-"));
+  bt_serial.println(airbeam_mac);
+  bt_buffer[0] = 0; 
+}
+ 
 uint8_t put_chr_buf(char c){
   // puts a character into the bt_buffer.
   // If the character is '\n' returns 1 (and doesn't put it into the buffer)
@@ -52,6 +287,7 @@ uint8_t put_chr_buf(char c){
       return 1;
   }
   if(bt_index >= BT_BUF_LEN - 2){
+    LOG_ERROR(F("BT buffer full"));
     return 0;
   }
   bt_buffer[bt_index] = c;
@@ -60,22 +296,24 @@ uint8_t put_chr_buf(char c){
   return 0;
 }
 
-void analyze_buf(){
+void analyze_buf(void){
   // Determines if the buffer contains the SS notation
-  if(bt_connected == -1){
+  if(bt_connected == NOT_CONNECTED){
     if(strncmp(bt_buffer, "SS=", 3) == 0){
-      bt_connected = 0;
-      Serial.println(F("1st Connect to BT"));
+      bt_connected = NOT_CONNECTED_AFTER_CONN;
+      LOG_INFO_LN(F("1st Connect to BT"));
     }
   }
   else{
-    if(strncmp(bt_buffer, "OK", 2) == 0){
+    if(strncmp(bt_buffer, "OK", 2) == 0)
+    {
     }
     else{
-      Serial.println(F("ERROR: Unknown response:"));
-      Serial.println(bt_buffer);
+      LOG_INFO_LN(F("ERROR: Unknown response: %s"), bt_buffer);
     }
-    bt_connected = 0;
+    /** When connected : we do not get ANY ok response */
+    bt_connected = NOT_CONNECTED_AFTER_CONN;
+    Timer1.start();
   }
   bt_buffer[0] = 0;
   bt_index = 0;
@@ -85,189 +323,53 @@ void update_bt_status(){
   // This should be called in the loop with at least a hundred milliescond delay before it is called again.
   // Determine if the BT module is connected or not.
   uint8_t temp = 0;
-  if(mySerial.available()){
-    while(mySerial.available()){
-      temp = put_chr_buf(mySerial.read());
+  if(bt_serial.available()){
+    while(bt_serial.available()){
+      temp = put_chr_buf(bt_serial.read());
+      LOG_DEBUG_LN("BT : buff length = %d - %s", strlen(bt_buffer), bt_buffer);
       if(temp){
         analyze_buf();
       }
     }
   }
   else{
-    if(bt_connected >= 0){
-      bt_connected = 1;
+    if(bt_connected == NOT_CONNECTED_AFTER_CONN || bt_connected == NOT_CONNECTED){
+      bt_connected = CONNECTED;
+      Timer1.stop();
+      led_status = HIGH;
+      digitalWrite(LEDPIN, led_status);
     }
   }
   
-  // clear excess
-  while(mySerial.available()){
-    mySerial.read();
-  }
+  clear_bt_serial();
   
   // Send out another signal for the next loop
-  mySerial.println(F("BC:UI=01"));
-  
-  // Indicate connection status via the LED
-  digitalWrite(LEDPIN, HIGH);
-  if(bt_connected < 1){
-    delay(100);
-    digitalWrite(LEDPIN, LOW);
-    delay(100);
-  }
+  bt_serial.println(F("BC:UI=01"));
 }
 
-String readBTMAC() {
-inData = "";
-if (mySerial.available() > 0) {
-Serial.println(mySerial.available());
-int h = mySerial.available();
-for (int i = 0; i < h; i++) {
-inData += (char)mySerial.read();
-}
-inData.remove(23 ,24);
-inData.remove(0, 11);
-}
-return inData;
+void readBTMAC(void) {
+  airbeam_mac = "";
+  if (bt_serial.available() > 0) {
+    int h = bt_serial.available();
+    for (int i = 0; i < h; i++) {
+      airbeam_mac += (char)bt_serial.read();
+    }
+    airbeam_mac.remove(23 ,24);
+    airbeam_mac.remove(0, 11);
+  }
 }
 
 void clear_bt_serial(){
-  while(mySerial.available()){
-    Serial.write(mySerial.read());
+  while(bt_serial.available()){
+    Serial.write(bt_serial.read());
   }
 }
 
-void setup() {
-  delay(3000);
-  Serial.begin(115200);
-  LOG_INIT(&Serial);
-
-  /** initialize Cairsens UART com - if called at end of setup, BT does not work */
-  cairsensSerial.begin(9600);
-  
-  mySerial.begin(115200);
-  mySerial.println("BC:BR=08");
-  delay(100);
-  
-  mySerial.begin(19200);
-  mySerial.println("BC:BR=08");
-  delay(100);
-  
-  mySerial.begin(9600);
-  mySerial.println("BC:BR=08");
-  delay(100);
-  
-  mySerial.begin(19200);
-  
-  mySerial.println("BC:FT=00,00,00,01,03,0000");  // It will never try to autoconnect and will always be in discoverable mode.
-  delay(100);
-  mySerial.println(("BC:AD"));
-  delay(100);
-  readBTMAC();
-  
-  mySerial.print(("BC:NM=AirBeam-"));
-  mySerial.println(inData);
-  bt_buffer[0] = 0;
-  
-  pinMode(LEDPIN, OUTPUT);
-  starttime = millis();
-  nox_starttime = millis();
+/** Called under interrupted context */
+void statusLedUpdate(void)
+{
+  // Update connection status via the LED
+  led_status =!led_status;
+  digitalWrite(LEDPIN, led_status);
 }
 
-void loop() {
-  analogvalue = analogvalue + analogRead(0);
-  analogcount++;
-
-  humi = dht.readHumidity();
-  cel = dht.readTemperature();
-  fah = ((cel * 9)/5) + 32;
- 
-  update_bt_status();
-  if ((millis()-starttime) > sampletime_ms)
-  {
-    starttime = millis(); // moved to create more regular periods.
-    analogvalue = analogvalue/analogcount;
-    analogtotal = (analogvalue*5.0)/1024;
-    hppcf = (240.0*pow(analogtotal,6) - 2491.3*pow(analogtotal,5) + 9448.7*pow(analogtotal,4) - 14840.0*pow(analogtotal,3) + 10684.0*pow(analogtotal,2) + 2211.8*(analogtotal) + 7.9623);
-    ugm3 = .518 + .00274 * hppcf;
-    if(ugm3 < 0){
-      ugm3 = 0;
-    }
-     
-    Serial.println();
-    Serial.print("AirBeam MAC: ");
-    Serial.print(inData);
-    Serial.print(" ");
-    Serial.print(fah);
-    Serial.print(F("F "));
-    Serial.print(cel);
-    Serial.print(F("C "));
-    Serial.print(humi);
-    Serial.print(F("RH "));
-    Serial.print(F(" Analog Total: "));
-    Serial.print(analogtotal);
-    Serial.print(F(" hppcf: "));
-    Serial.print(hppcf);
-    Serial.print(F(" ugm^3: "));
-    Serial.print(ugm3);
-    Serial.println();
-    
-    analogvalue = 0;
-    analogcount = 0;
-    //lowpulseoccupancy = 0;
-    
-    if(bt_connected > 0){
-      //mySerial.print(analogtotal);
-      //mySerial.print((";AirBeam:"));
-      //mySerial.print(inData);
-      //mySerial.print((";AirBeam-PM-V;Particulate Matter;PM;Volts;V;0;1;2;3;4"));
-      //mySerial.print("\n");
-      
-      //mySerial.print(hppcf);
-      //mySerial.print((";AirBeam:"));
-      //mySerial.print(inData);
-      //mySerial.print((";AirBeam-PM-hppcf;Particulate Matter;PM;hundreds of particles per cubic foot;hppcf;0;3000;8000;13000;26000"));
-      //mySerial.print("\n");
-      
-      mySerial.print(ugm3);
-      mySerial.print((";AirBeam:"));
-      mySerial.print(inData);
-      mySerial.print((";AirBeam-PM;Particulate Matter;PM;micrograms per cubic meter;µg/m³;0;12;35;55;150"));
-      mySerial.print("\n");
-      
-      mySerial.print(fah);
-      mySerial.print((";AirBeam:"));
-      mySerial.print(inData);
-      mySerial.print((";AirBeam-F;Temperature;F;degrees Fahrenheit;F;0;25;50;75;100"));
-      mySerial.print("\n");
-   
-      //mySerial.print(cel);
-      //mySerial.print((";AirBeam:"));
-      //mySerial.print(inData);
-      //mySerial.print((";AirBeam-C;Temperature;C;degrees Celsius;C;0;10;20;30;40"));
-      //mySerial.print("\n");
-      
-      mySerial.print(humi);
-      mySerial.print((";AirBeam:"));
-      mySerial.print(inData);
-      mySerial.print((";AirBeam-RH;Humidity;RH;percent;%;0;25;50;75;100"));
-      mySerial.print("\n");
-      
-    }
-    
-    }
-  if ((millis() - nox_starttime) > NOX_SAMPLE_TIME)
-  {
-    uint8_t nox_val = 0;
-    if(cairsens.getInstantValue(nox_val) == CairsensUART::NO_ERROR)
-    {
-      Serial.print("Current NOX value = ");
-      Serial.print(nox_val/10.0);
-      Serial.println("ppm");
-    }
-    else
-    {
-      Serial.println("Could not get Cairsens NOX value");
-    }
-    nox_starttime = 0;
-  }
-}
